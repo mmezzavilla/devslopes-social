52ff536e08eb7dc5b0dd057a647f09231c2b7d5f
Marco Mezzavilla <mezzavilla.marco@gmail.com>
2016-12-28 19:22:33 -0500

    Initial Commit

---
+++ devslopes-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by Marco Mezzavilla on 12/28/16.
+//  Copyright © 2016 Marco Mezzavilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
